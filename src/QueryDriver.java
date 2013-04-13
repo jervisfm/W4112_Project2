9d99388bad2257171ccb3a451247d77ff9a19906
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-04-13 00:27:51 -0400

    verifying parsing code works

--- src/QueryDriver.java
+++ src/QueryDriver.java
@@ -7,8 +7,18 @@ public class QueryDriver
 	{
 		String queryPath = args[0];
 		String configPath = args[1];
-
+				
 		ArrayList<double[]> query = QueryParser.parseQuery(queryPath);
 		Properties config = QueryParser.parseConfig(configPath);
+		printList(query);
+		System.out.println(config);
+	}
+	private static void printList(ArrayList<double[]> query) {
+		for(double[] array : query) {
+			for(double d : array)  {
+				System.out.printf("%f, ", d);
+			}
+			System.out.println("");
+		}
 	}
 }
