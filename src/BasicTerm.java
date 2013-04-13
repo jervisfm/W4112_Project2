37d9d90e07bcbc40865dcddd84f57a70df15581e
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-04-13 03:02:05 -0400

    add print out for basic term

--- src/BasicTerm.java
+++ src/BasicTerm.java
@@ -6,8 +6,18 @@
 public class BasicTerm  {
 	public String function; 
 	public String argument; 
-	public BasicTerm(String function, String argument) {
+	public double selectivity; 
+	public BasicTerm(String function, String argument, double selectivity) {
 		this.function = function;
 		this.argument = argument;
+		this.selectivity = selectivity; 
+	}
+	
+	public String toString() {
+		StringBuffer sb = new StringBuffer(); 
+		sb.append(function); 
+		sb.append("(" + argument + ")");
+		sb.append("@" + selectivity); 
+		return sb.toString();
 	}
 }
