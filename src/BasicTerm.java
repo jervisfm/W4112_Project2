7b4963c59b7e90536c5647a79924ef09854a0bbc
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-04-13 02:36:18 -0400

    add basic term

---
+++ src/BasicTerm.java
@@ -0,0 +1,13 @@
+
+/**
+ * Represents a single function condition. 
+ * @author Jervis
+ */
+public class BasicTerm  {
+	public String function; 
+	public String argument; 
+	public BasicTerm(String function, String argument) {
+		this.function = function;
+		this.argument = argument;
+	}
+}
