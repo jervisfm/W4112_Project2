017a4d9536f0c4e2c2eb3ecb7dba3fb4172aa430
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-04-13 17:46:33 -0400

    update logicalandterm with subset #

--- src/LogicalAndTerm.java
+++ src/LogicalAndTerm.java
@@ -9,6 +9,11 @@ import java.util.ArrayList;
 public class LogicalAndTerm {
 
 	private ArrayList<BasicTerm> data; 
+	/**
+	 * We use to label in what array index this subset is being stored in. 
+	 * It helps us to do easy look ups. 
+	 */
+	private int subset_no; 
 	public LogicalAndTerm(BasicTerm term) {
 		this.data  = new ArrayList<BasicTerm>();
 		data.add(term);		
@@ -27,6 +32,18 @@ public class LogicalAndTerm {
 	
 	public BasicTerm get(int i ) {
 		return data.get(i);
+	}
+	
+	public void setSubsetNo(int n) {
+		if (n < 0) {
+			throw new IllegalArgumentException(
+					"subset no can't be negative: " + n);
+		}
+		this.subset_no = n; 
+	}
+	
+	public int getSubsetNo() {
+		return this.subset_no; 
 	}
 	
 	public int size() {
