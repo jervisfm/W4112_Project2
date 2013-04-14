3e101318006e61996dfb32667ef2a3d934bbadc3
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-04-13 20:03:37 -0400

    update branching and plan

--- src/BranchingAndPlan.java
+++ src/BranchingAndPlan.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * 
  * @author Jervis Muindi
@@ -7,5 +9,11 @@ public class BranchingAndPlan extends Plan {
 	
 	public BranchingAndPlan() {
 		super(); 
+	}
+	
+	public BranchingAndPlan(Plan left, Plan right, ArrayList<BasicTerm> terms) {
+		this.left = left;
+		this.right = right; 
+		this.terms = terms;  
 	}
 }
