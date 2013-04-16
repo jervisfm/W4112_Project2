443d53673d10b78e514d843b358d8d4fa7f82110
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-04-16 02:01:58 -0400

    add pair class

---
+++ src/Pair.java
@@ -0,0 +1,31 @@
+
+/**
+ * Represents a pair of numeric values. 
+ * @author Jervis
+ *
+ */
+public class Pair {
+	public double x;
+	public double y; 
+		
+	public Pair(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public double getX() {
+		return x;
+	}
+	
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+}
