3a6a9d6a165b3e199fe6b77cc8baeeb335045604
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-04-26 14:57:13 -0400

    work on method to compile results together froma file

--- Benchmark/Results/compile.py
+++ Benchmark/Results/compile.py
@@ -2,6 +2,8 @@
 
 __author__ = 'Jervis Muindi'
 
+import argparse
+
 class Result:
 	"""
 		Class for storing the benchmark result
@@ -47,6 +49,78 @@ class Person:
 		return p
 
 
+def read_file_to_string(file_path):
+	f = open(file_path)
+	line = f.readline().strip()
+	result = line
+
+	while line:
+        	line = f.readline().strip()
+	        result += '\n'
+        	result += line
+	f.close()
+	return result
+
+def compile_result_from_file(file):
+	""" 
+		Compile all the results in the given file
+		to a single record by averaging all the values 
+		found
+	"""
+	records_string = read_file_to_string(file)
+	MARKER = "Loop start!\nLoop stop!\n"
+	
+	records = records_string.split(MARKER)
+	r = Result()
+	record_count = 0
+	for record in records:
+		if not record: # Skip empty records
+			continue
+		items = record.split('\n')
+		for item in items:
+			if 'elapsed time' in item.lower():
+				# Format is like this
+				# elapsed time : 123123 seconds
+				num = float(item.split(':')[1].split(' ')[0])
+				r.elapsed_time += num
+			elif 'cpu cycles' in item.lower():
+				# Format is like this
+				# cpu cycles : 123123
+				num = float(item.split(':')[1])
+				r.cpu_cyles += num
+			elif 'instructions' in item.lower():
+				num = float(item.split(':')[1])
+				r.instructions += num
+			elif 'ipc' in item.lower():
+				num = float(item.split(':')[1])
+				r.ipc += num
+			elif 'branch misses' in item.lower():
+				num = float(item.split(':')[1])
+				r.branch_misses += num
+			elif 'branch instructions' in item.lower():
+				num = float(item.split(':')[1])
+				r.branch_instructions += num
+			elif 'branch mispred' in item.lower():
+				# Format is like this
+				# branch mispred rate: 1.12312%
+				percentage = item.split(':')[1]
+				num = float(percentage.strip()[:-1]) # remove percentage
+				r.branch_mispredict_rate += num				
+			else:
+				pass
+		
+		record_count += 1
+	print 'We found no of  Records = %s' % record_count
+	# Average out the results
+	r.elpased_time /= float(record_count)
+	r.cpu_cycles /= float(record_count)
+	r.instructions /= float(record_count)
+	r.ipc /= float(record_count)
+	r.branch_misses /= float(record_count)
+	r.branch_instructions /= float(record_count)
+	r.branch_mispredict_rate /= float(record_count)
+	return r
+	
 def main():
 	
 	print "hi you"
@@ -56,6 +130,8 @@ def main():
 	p.age = 17
 	p3 = p + p2
 	print p3
+	file =  'result_land_q1_0.txt' 
+	print compile_result_from_file(file)
 
 if __name__ == '__main__':
 	main()
