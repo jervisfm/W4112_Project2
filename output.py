1f7b64e8903ba6a6663bfee5e74a8c9131d79984
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-04-25 20:13:03 -0400

    add code to parse output file

--- output.py
+++ output.py
@@ -14,6 +14,20 @@ def read_file_to_string(file_path):
         result += line
     return result
 
+def get_optimal_code(file_path):
+
+    CODE_SPLIT = '=================================================================='
+    CODE_MARKER = '------------------------------------------------------------------'
+
+    file = read_file_to_string(file_path)
+
+    code_sections = file.split(CODE_SPLIT)
+    for code_section in code_sections:
+        if len(code_section) == 0:
+            continue
+
+        lines = code_section.split(CODE_MARKER)
+        print lines[1]
 
 
 
@@ -23,7 +37,7 @@ def main():
     args = vars(parser.parse_args())
 
     file = args['file']
-    print read_file_to_string(file)
+    print get_optimal_code(file)
 
 
 if __name__ == '__main__':
