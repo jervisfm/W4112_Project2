f3b278e054fe5bb57cf4c0947c8496058a971be7
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-04-26 17:58:37 -0400

    start work on reading compiled results

--- Benchmark/Results/plot.py
+++ Benchmark/Results/plot.py
@@ -1,12 +1,28 @@
 #! /usr/bin/python
 
 import argparse
+from compile import parse_record
+from compile import read_file_to_string
 
 def main():	
 	
 	parser = argparse.ArgumentParser(description='Plots compiled benchmark results')
 	
-	pass
+	file = 'compile_result_land_q1.txt'
+	
+	file_string = read_file_to_string(file)
+	record_marker = '===='
+	records = file_string.split(record_marker)
+	results = []
+	for record in records:
+		if not record: # skip empty records
+			continue
+
+		r = parse_record(record)
+		results.append(r)
+
+	
+	print results[]
 
 if __name__ == '__main__':
 	main()
