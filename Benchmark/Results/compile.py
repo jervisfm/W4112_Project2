20ee494705f4839572c0105bcea0de8f84ba74ba
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-04-26 14:14:16 -0400

    update result class

--- Benchmark/Results/compile.py
+++ Benchmark/Results/compile.py
@@ -3,6 +3,9 @@
 __author__ = 'Jervis Muindi'
 
 class Result:
+	"""
+		Class for storing the benchmark result
+	"""
 	def __init__(self):
 		self.elapsed_time = 0
 		self.cpu_cycles = 0
@@ -11,8 +14,20 @@ class Result:
 		self.branch_misses = 0
 		self.branch_instructions = 0
 		self.branch_mispredict_rate = 0
+		self.name = ''
 
-	
+	def __str__(self):
+		result = ''
+		result += 'Benchmark name: %s\n' % self.name
+		result += 'Elapsed Time: %s\n' % self.elapsed_time
+		result += 'Cpu Cycles: %s\n' % self.cpu_cycles
+		result += 'Instructions: %s\n' % self.instructions
+		result += 'IPC : %s\n' % self.ipc
+		result += 'Branch Misses: %s\n' % self.branch_misses
+		result += 'Branch Instructions: %s\n' % self.branch_instructions
+		result += 'Branch MisPredict Rate: %s\n' % self.branch_mispredict_rate
+		return result
+
 
 class Person:
 	def __init__(self):
@@ -21,6 +36,7 @@ class Person:
 		self.sex = 'Male'
 
 	def __str__(self):
+		
 		return 'Name:%s\nAge:%s\nSex:%s' %(self.name, self.age, self.sex)
 
 	def __add__(self, other):
