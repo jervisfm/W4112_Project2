8f40a0bb10084a4933a4bda42e924f4b94e2c511
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-04-26 17:09:20 -0400

    refactor result.py

--- Benchmark/Results/compile.py
+++ Benchmark/Results/compile.py
@@ -3,32 +3,8 @@
 __author__ = 'Jervis Muindi'
 
 import argparse
+import result
 
-class Result:
-	"""
-		Class for storing the benchmark result
-	"""
-	def __init__(self):
-		self.elapsed_time = 0
-		self.cpu_cycles = 0
-		self.instructions = 0
-		self.ipc = 0
-		self.branch_misses = 0
-		self.branch_instructions = 0
-		self.branch_mispredict_rate = 0
-		self.name = ''
-
-	def __str__(self):
-		result = ''
-		result += 'Benchmark name: %s\n' % self.name
-		result += 'Elapsed Time: %s\n' % self.elapsed_time
-		result += 'Cpu Cycles: %s\n' % self.cpu_cycles
-		result += 'Instructions: %s\n' % self.instructions
-		result += 'IPC : %s\n' % self.ipc
-		result += 'Branch Misses: %s\n' % self.branch_misses
-		result += 'Branch Instructions: %s\n' % self.branch_instructions
-		result += 'Branch MisPredict Rate: %s\n' % self.branch_mispredict_rate
-		return result
 
 def read_file_to_string(file_path):
 	f = open(file_path)
