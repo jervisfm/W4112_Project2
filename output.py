8c77fd06520ed60823f35c44b9b0b6624c494228
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-04-25 20:00:29 -0400

    add read file method

--- output.py
+++ output.py
@@ -1,10 +1,30 @@
 #! /usr/bin/python
 
-def test():
-	print 'hi me you'
+__author__ = 'Jervis Muindi'
+
+import argparse
+
+def read_file_to_string(file_path):
+    f = open(file_path)
+    line = f.readline().strip()
+    result = line
+    while line:
+        line = f.readline().strip()
+        result += '\n'
+        result += line
+    return result
+
+
+
 
 def main():
-	test()
+    parser = argparse.ArgumentParser(description='Generates C-program source code that measures branch mispredictions from a file listing optimal plans for various selectivities')
+    parser.add_argument('-f','--file', help='File to read', required=True)
+    args = vars(parser.parse_args())
+
+    file = args['file']
+    print read_file_to_string(file)
+
 
 if __name__ == '__main__':
 	main()
