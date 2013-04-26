df4713b29acbccfcfca06885ed200e4aefe86e05
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-04-26 19:22:00 -0400

    add method to extract the y-values from the results

--- Benchmark/Results/plot.py
+++ Benchmark/Results/plot.py
@@ -3,6 +3,59 @@
 import argparse
 from compile import parse_record
 from compile import read_file_to_string
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+
+__author__ = 'Jervis Muindi'
+
+
+def get_x_points():
+	"""
+		Get the x-points to use in the graph plot
+	"""
+	ans = []
+	for i in xrange(11):
+		ans.append(i * 0.1)
+	return ans
+
+
+def check_results(results):
+	if not results:
+		raise ValueError('results cannot be empty')
+
+def get_mispredict_rate(results):
+	ans = []
+	check_results(results)
+	for result in results:
+		val = result.branch_mispredict_rate
+		ans.append(val)
+	return ans		
+
+def get_elapsed_time(results):
+	ans = []
+	check_results(results)
+	for result in results:
+		val = result.elapsed_time
+		ans.append(val)
+
+	return ans
+
+def get_ipc(results):
+	ans = []
+	check_results(results)
+	for result in results:
+		val = result.ipc
+		ans.append(val)
+	return ans
+	
+
+def plot_benchmark_result(results):
+	title = ''
+	if results:
+		title = results[0].name.split('|')[1].strip()
+	for result in results:
+		
+
 
 def main():	
 	
@@ -24,7 +77,7 @@ def main():
 
 
 	print 'we have this many results: %d' % len(results)
-	#print results
+	print results
 
 if __name__ == '__main__':
 	main()
