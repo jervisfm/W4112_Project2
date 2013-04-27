9e10427f9ef69de51b48ab588b01bbd6eb1ab1f3
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-04-26 23:57:06 -0400

    plot elapsed time

--- Benchmark/Results/group_plot.py
+++ Benchmark/Results/group_plot.py
@@ -50,31 +50,91 @@ def init_fig(xlabel, ylabel, title):
 	return fig
 
 
+
+def plot_combined_results(x, y_land, y_nb, y_pand, y_opt):
+	plt.plot(x, y_land, 'ro--', markersize=10, label='Logical And')
+        plt.plot(x, y_nb, 'g2--', markersize=20, markeredgewidth=2, label='No Branch')
+        plt.plot(x, y_pand, 'b+--', markersize=20, markeredgewidth=2, label='Branching And')
+        plt.plot(x, y_opt, 'k', markersize=5, markeredgewidth=5, linewidth=3, label='Optimized Plan')
+        plt.legend()
+ 
+
 def plot_mispredict_rate(dict):
+
+        # Query 1
+        title = 'Branch MisPrediction Rate Query 1'
+        ylabel = '% Branch Misprediction Rate'
+        xlabel = 'Combined Selectivity'
+        fig = init_fig(xlabel, ylabel, title)
+        fname = 'combined_graph_q1_mispredict_rate.png'
+
+        # Get the X-Points
+        x = get_x_points()
+
+        # Get the Y-Points
+        y_land_q1 = get_mispredict_rate(dict['compiled_result_land_q1'])
+        y_nb_q1 = get_mispredict_rate(dict['compiled_result_nb_q1'])
+        y_pand_q1 = get_mispredict_rate(dict['compiled_result_pand_q1'])
+        y_opt_q1 = get_mispredict_rate(dict['compiled_result_optimal_q1'])
+
+        #Plot the Combined Results
+        plot_combined_results(x, y_land_q1, y_nb_q1, y_pand_q1, y_opt_q1)
+        fig.savefig(fname)
+
+
+        # Query 2
+        title = 'Branch MisPrediction Rate Query 2'
+        fig = init_fig(xlabel, ylabel, title)
+        fname = 'combined_graph_q2_mispredict_rate.png'
+
+        # Get the Y-Points
+        y_land_q2 = get_mispredict_rate(dict['compiled_result_land_q2'])
+        y_nb_q2 = get_mispredict_rate(dict['compiled_result_nb_q2'])
+        y_pand_q2 = get_mispredict_rate(dict['compiled_result_pand_q2'])
+        y_opt_q2 = get_mispredict_rate(dict['compiled_result_optimal_q2'])
+
+        #Plot the Combined Results
+        plot_combined_results(x, y_land_q2, y_nb_q2, y_pand_q2, y_opt_q2)
+        fig.savefig(fname)
+
+def plot_elapsed_time(dict):
 	
 	# Query 1
-	title = 'Branch MisPrediction Rate Query 1'
-	ylabel = '% Branch Misprediction Rate'
+	title = 'Elapsed Time Query 1'
+	ylabel = 'Elapsed Time (seconds)'
 	xlabel = 'Combined Selectivity'
 	fig = init_fig(xlabel, ylabel, title)
-	fname = 't.png'
+	fname = 'combined_graph_q1_elapsed_time.png'
 	
 	# Get the X-Points
 	x = get_x_points()
 
 	# Get the Y-Points
- 	y_land_q1 = get_mispredict_rate(dict['compiled_result_land_q1'])
-	y_nb_q1 = get_mispredict_rate(dict['compiled_result_nb_q1'])
-	y_pand_q1 = get_mispredict_rate(dict['compiled_result_pand_q1'])	
-	y_opt_q1 = get_mispredict_rate(dict['compiled_result_optimal_q1'])
+ 	y_land_q1 = get_elapsed_time(dict['compiled_result_land_q1'])
+	y_nb_q1 = get_elapsed_time(dict['compiled_result_nb_q1'])
+	y_pand_q1 = get_elapsed_time(dict['compiled_result_pand_q1'])	
+	y_opt_q1 = get_elapsed_time(dict['compiled_result_optimal_q1'])
 
 	#Plot the Combined Results	
-	plt.plot(x, y_land_q1, 'ro--', markersize=10, label='Logical And')
-	plt.plot(x, y_nb_q1, 'g2--', markersize=20, markeredgewidth=2, label='No Branch')
-	plt.plot(x, y_pand_q1, 'b+--', markersize=20, markeredgewidth=2, label='Branching And')
-	plt.plot(x, y_opt_q1, 'k', markersize=5, markeredgewidth=5, linewidth=3, label='Optimized Plan') 
-	plt.legend()
+	plot_combined_results(x, y_land_q1, y_nb_q1, y_pand_q1, y_opt_q1)
 	fig.savefig(fname)
+	
+
+	# Query 2
+	title = 'Elapsed Time QQuery 2'
+	fig = init_fig(xlabel, ylabel, title)
+	fname = 'combined_graph_q2_elapsed_time.png'
+
+	# Get the Y-Points
+        y_land_q2 = get_elapsed_time(dict['compiled_result_land_q2'])
+        y_nb_q2 = get_elapsed_time(dict['compiled_result_nb_q2'])
+        y_pand_q2 = get_elapsed_time(dict['compiled_result_pand_q2'])
+        y_opt_q2 = get_elapsed_time(dict['compiled_result_optimal_q2'])
+
+        #Plot the Combined Results
+        plot_combined_results(x, y_land_q2, y_nb_q2, y_pand_q2, y_opt_q2)
+        fig.savefig(fname)
+
 	
 	
 		
@@ -103,9 +163,10 @@ def main():
 	plot_mispredict_rate(dict)
 
 	# Plot All ELapsed Time Graphs Together
-
+	plot_elapsed_time(dict)
 
 	# Plot the Instruction Per Clock Cycle Together
+	
 
 	print 'Our dict of size == %d' % len(dict)
 	#print dict
