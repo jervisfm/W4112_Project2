b6ca3a2041b100a68ff90b32084661d2dd3f14ce
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-04-26 21:59:25 -0400

    refactor group plot

--- Benchmark/Results/group_plot.py
+++ Benchmark/Results/group_plot.py
@@ -12,6 +12,31 @@ from plot import get_graph_title
 
 
 
+def get_dict_from_compiled_files(files_to_plot): 
+	"""
+		Returns a dictionary of compiled_file -> All Result records
+		from the list of the given files
+	"""
+	if not files_to_plot:
+		raise ValueError('files cannot be None')
+	
+	dict = {}
+        record_marker = '===='
+        for file in files_to_plot:
+                file_string = read_file_to_string(file)
+                records = file_string.split(record_marker)
+                results = []
+
+                for record in records:
+                        if not record.strip(): # skip empty records
+                                continue
+                        r = parse_record(record)
+                        results.append(r)
+                key = file[:-4] # skip the extension
+                dict[key] = results
+	return dict
+
+
 def main():
 
 	parser = argparse.ArgumentParser(description='Group Plots from compiled results')
@@ -22,25 +47,13 @@ def main():
 			 'compiled_result_land_q1.txt', 'compiled_result_land_q2.txt', 
 			 'compiled_result_pand_q1.txt', 'compiled_result_pand_q2.txt',
 			 'compiled_result_optimal_q1.txt', 'compiled_result_optimal_q2.txt']
-	file_strings = []
 
-	dict = {}
-	record_marker = '===='
-	for file in files_to_plot:
-		file_string = read_file_to_string(file)
-		records = file_string.split(record_marker)
-		results = []
-		
-		for record in records:
-			if not record.strip(): # skip empty records
-				continue
-			r = parse_record(record)
-			results.append(r)
-		key = file[:-4] # skip the extension
-		dict[key] = results
-	
+
+
+
+	dict = get_dict_from_compiled_files(files_to_plot)	
 	print 'Our dict of size == %d' % len(dict)
-	
+	#print dict
 
 	
 
