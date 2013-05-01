c0203f15c6f7129722aade59c746b50079ebf88a
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-04-30 23:32:10 -0400

    update compile script

--- Benchmark/Results/compile-all.sh
+++ Benchmark/Results/compile-all.sh
@@ -4,24 +4,28 @@
 echo "Compiling logical and base results ..."
 ./compile.py -p result_land_q1
 ./compile.py -p result_land_q2
+./compile.py -p result_land_q3
 echo "done"
 
 # Compile Branching And base result
 echo "Compile branching and base results"
 ./compile.py -p result_pand_q1
 ./compile.py -p result_pand_q2
+./compile.py -o result_pand_q3
 echo "done"
 
 # Compile No-Branch Base result
 echo "compile no-branch base result..."
 ./compile.py -p result_nb_q1
 ./compile.py -p result_nb_q2
+./compile.py -p result_nb_q3
 echo "done"
 
-echo "Compile optimal result q1+q2..."
-# Compile Optimal Results Q1 + Q2
+echo "Compile optimal result q1+q2 + q3..."
+# Compile Optimal Results Q1 + Q2 + q3...
 ./compile.py -p result_optimal_q1
 ./compile.py -p result_optimal_q2
+./compile.py -p result_optimal_q3
 echo "done"
 
 echo "All Compilations done"
